3e8b6ad77dcdd91fc14f6f520579fce0881ee192
Pranusha <narsicloud@gmail.com>
2024-09-19 21:44:33 -0500

    class1-studio-datatypes

---
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -0,0 +1,12 @@
+package org.launchcode;
+
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a radius: ");
+        Double radius = input.nextDouble();
+        System.out.println("The Area of radius "+ radius + " is " + Circle.getArea(radius));
+    }
+}
