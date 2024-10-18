8ece9af0babbda544e0ad4cf38524d25b3890d54
Pranusha <narsicloud@gmail.com>
2024-10-17 20:22:30 -0500

    unit testing studio

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -10,4 +10,61 @@ class BalancedBracketsTest {
     public void emptyTest() {
         assertEquals(true, true);
     }
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void balancedBracketFullReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    @Test
+    public void balanceBracketsPartialReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+
+    }
+
+    @Test
+    public void balanceBracketsPartialFirstBracketOnlyTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]LaunchCode"));
+    }
+
+    @Test
+    public void balanceBracketsEmptyStringTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void balanceBracketsOpenBracketOnlyFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+
+    }
+
+    @Test
+    public void balanceBracketsWrongOrderFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
+
+    @Test
+    public void balancedBracketsClosedBracketOnlyFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void unbalancedBracketsReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void unbalancedBracketsUnevenOpenReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode[]"));
+    }
+
+    @Test
+    public void unbalancedBracketsUnevenClosedReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]LaunchCode[]"));
+    }
+
 }
