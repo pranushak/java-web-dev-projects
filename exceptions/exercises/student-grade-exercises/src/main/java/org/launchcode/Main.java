74711dbd123a07f22bc005b2a813e94e2cfbd728
Pranusha <narsicloud@gmail.com>
2024-11-20 12:35:03 -0600

    exceptions

--- exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
@@ -1,9 +1,10 @@
 package org.launchcode;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws StudentException {
         // Test out your Divide() function!
 
         HashMap<String, String> studentFiles = new HashMap<>();
@@ -13,15 +14,40 @@ public class Main {
         studentFiles.put("Stefanie", "CoolProgram.java");
 
         // Test out your CheckFileExtension() function!
+
+        Main.divide(2,1);
+       // System.out.println();
+        for(Map.Entry<String, String> file : studentFiles.entrySet()){
+            String key = file.getKey();
+            String value = file.getValue();
+
+            try{
+                int points = checkFileExtension(value);
+                System.out.println(key + "got" + points + "points");
+            }catch (StudentException e){
+                System.out.println("Error " + key + e.getMessage());
+            }
+        }
+        Main.checkFileExtension( "Program.java");
     }
 
-    public static void Divide(int x, int y)
-    {
+    public static void divide(int x, int y) {
         // Write code here!
+            try {
+                if (y <= 0) {
+                    throw new ArithmeticException("denominator should not zero!");
+                }
+                System.out.println(x/y);
+            } catch (ArithmeticException e) {
+                System.out.println("Error " + e.getMessage());
+            }
     }
 
-    public static int CheckFileExtension(String fileName)
-    {
-        // Write code here!
+    public static int checkFileExtension(String fileName) throws StudentException {
+        if(fileName == null || fileName.isEmpty()){
+            throw new StudentException("File name cannot be null or empty");
+        }
+        return fileName.endsWith(".java") ? 1 : 0;
     }
+
 }
---
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/StudentException.java
@@ -0,0 +1,7 @@
+package org.launchcode;
+
+public class StudentException extends Exception{
+    public StudentException(String message){
+        super(message);
+    }
+}
